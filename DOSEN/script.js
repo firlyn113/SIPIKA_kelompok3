14c6685701404f12789ac176955d702d9d0c66dc
wandalevina <wandalevina4@gmail.com>
2026-09-22 15:07:26 +0700

    Pengerjaan role dosen [wanda]

---
+++ DOSEN/script.js
@@ -0,0 +1,9 @@
+function setujui() {
+    alert("Pengajuan berhasil disetujui");
+    window.location.href = "pengajuan.html";
+}
+
+function tolak() {
+    alert("Pengajuan berhasil ditolak");
+    window.location.href = "pengajuan.html";
+}
